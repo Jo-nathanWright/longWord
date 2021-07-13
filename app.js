3238cc1eadb28eb1b2432d5efe03c83d505e0466
Jonathan Wright <j.wright72127@gmail.com>
2021-07-13 09:19:00 -0600

    Explained How the code works with comments

--- app.js
+++ app.js
@@ -4,15 +4,15 @@
 
 function longestWord(str) {
     // your code here
-    let words = str.split(' ');
-    let maxLength = 0;
-    for (let i = 0; i < words.length; i++) {
-        if (words[i].length > maxLength) {
-            maxLength = words[i].length;
-            let longWord = words[i]
-            console.log(longWord)
+    let words = str.split(' '); //Splits the string into an array, with each word being an differnet string
+    let maxLength = 0; // sets the maxlength to zero
+    for (let i = 0; i < words.length; i++) { //runs while i is less than then length of the array
+        if (words[i].length > maxLength) { //checks to see if a strings length is greater than the maxLength (which would start at zero)
+            maxLength = words[i].length; //updates the maxLength with the length of the longest word(Which here would start at 4, switch to 8 and ends on 9)
+            let longWord = words[i] // updates the longWord to be the word that was the longest
+            console.log(longWord) //console logs longWord
         }
     }
 }
 
-longestWord("This is a very long sentence that has many a word but only one ginormous one!")
+longestWord("This is a very long sentence that has many a word but only one ginormous one!") //seting up the string for the function
